88beee18af45578b7f8730168cd26bc7a057600c
Tracy Reuther <tracyreuther@gmail.com>
2019-09-08 18:43:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,8 @@ class App < Sinatra::Base
   get '/friends' do
     # Write your code here!
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
-
+    % @friends.each do |friend| %>
+      <h2><%= friend %></h2>
+    <% end %>
   end
 end
